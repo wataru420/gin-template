1efe89346fd3389e8f1cf6780b89f840ddb1b8b8
植木 友浩 <ueki_tomohiro@cyberagent.co.jp>
2015-11-13 16:30:32 +0900

    件数の制限

--- src/totec/service/postService.go
+++ src/totec/service/postService.go
@@ -1,17 +1,19 @@
 package service
+
 import (
 	"log"
 	"net/http"
-	"github.com/gin-gonic/gin"
-	"totec/models"
 	"strings"
+	"totec/models"
+
+	"github.com/gin-gonic/gin"
 )
 
 var postDao = &models.PostDao{}
 
-type PostService struct {}
+type PostService struct{}
 
-func (*PostService) GetEndpoint(c *gin.Context)  {
+func (*PostService) GetEndpoint(c *gin.Context) {
 	id := c.Param("id")
 
 	post, err := postDao.Get(id)
@@ -21,32 +23,32 @@ func (*PostService) GetEndpoint(c *gin.Context)  {
 	}
 
 	c.JSON(http.StatusOK, gin.H{
-		"postId": post.Id,
-		"postNo": post.DateTime,
-		"postUserId": post.UserId,
-		"postItemId": post.ItemId,
+		"postId":        post.Id,
+		"postNo":        post.DateTime,
+		"postUserId":    post.UserId,
+		"postItemId":    post.ItemId,
 		"postItemScore": post.ItemScore,
 		"postItemState": post.ItemState,
 		"postLikeUsers": post.LikeUsers,
-		"postTags": post.Tags,
+		"postTags":      post.Tags,
 	})
 }
 
-func (*PostService) GetWebEndpoint(c *gin.Context)  {
+func (*PostService) GetWebEndpoint(c *gin.Context) {
 	id := c.Param("id")
 
-	post , err := postDao.Get(id)
+	post, err := postDao.Get(id)
 	if err != nil {
 		log.Println("error")
 		c.String(http.StatusInternalServerError, err.Error())
 	}
 
-	item , _ := itemDao.Get(post.ItemId)
+	item, _ := itemDao.Get(post.ItemId)
 
 	var likeUsers = []models.User{}
-	for _, fid := range strings.Split(post.LikeUsers,",") {
-		friend , _ := userDao.Get(fid)
-		likeUsers = append(likeUsers,friend)
+	for _, fid := range strings.Split(post.LikeUsers, ",") {
+		friend, _ := userDao.Get(fid)
+		likeUsers = append(likeUsers, friend)
 	}
 
 	posts, _ := postDao.FindByPostUserId(post.UserId, 8)
@@ -63,17 +65,17 @@ func (*PostService) GetWebEndpoint(c *gin.Context)  {
 		userItems = append(userItems, item)
 	}
 
-
 	user, _ := userDao.Get(post.UserId)
 
 	c.HTML(http.StatusOK, "postDetail.tmpl", gin.H{
-		"title": "Main website",
-		"post": post,
-		"item": item,
-		"user": user,
-		"likeUsers": likeUsers,
-		"postImages": postItems,
-		"userImages": userItems,
-		"tags": strings.Split(item.Tags,","),
+		"title":         "Main website",
+		"post":          post,
+		"item":          item,
+		"user":          user,
+		"likeUserCount": len(likeUsers),
+		"likeUsers":     likeUsers[0:3],
+		"postImages":    postItems,
+		"userImages":    userItems,
+		"tags":          strings.Split(item.Tags, ","),
 	})
 }
--- src/totec/service/userService.go
+++ src/totec/service/userService.go
@@ -1,18 +1,19 @@
 package service
+
 import (
 	"log"
 	"net/http"
-	"github.com/gin-gonic/gin"
-	"totec/models"
 	"strings"
+	"totec/models"
+
+	"github.com/gin-gonic/gin"
 )
 
 var userDao = &models.UserDao{}
 
-type UserService struct {}
+type UserService struct{}
 
-func (*UserService) GetEndpoint(c *gin.Context)  {
-
+func (*UserService) GetEndpoint(c *gin.Context) {
 
 	id := c.Param("id")
 
@@ -23,36 +24,36 @@ func (*UserService) GetEndpoint(c *gin.Context)  {
 	}
 
 	c.JSON(http.StatusOK, gin.H{
-		"userId": user.Id,
-		"userNo": user.No,
+		"userId":         user.Id,
+		"userNo":         user.No,
 		"userPublicSore": user.PublicScore,
-		"userFriends": strings.Split(user.Friends,","),
-		"userImage": user.Image,
+		"userFriends":    strings.Split(user.Friends, ","),
+		"userImage":      user.Image,
 	})
 }
 
-func (*UserService) GetWebEndpoint(c *gin.Context)  {
+func (*UserService) GetWebEndpoint(c *gin.Context) {
 	id := c.Param("id")
 
-	user , err := userDao.Get(id)
+	user, err := userDao.Get(id)
 	if err != nil {
 		log.Println("error")
 		c.String(http.StatusInternalServerError, err.Error())
 	}
 
 	var friends = []models.User{}
-	for _, fid := range strings.Split(user.Friends,",") {
-		friend , _ := userDao.Get(fid)
-		friends = append(friends,friend)
+	for _, fid := range strings.Split(user.Friends, ",") {
+		friend, _ := userDao.Get(fid)
+		friends = append(friends, friend)
 	}
 
-	items, err := itemDao.FindByPostUserId(id,8)
+	items, err := itemDao.FindByPostUserId(id, 8)
 	if err != nil {
 		log.Println("error")
 		c.String(http.StatusInternalServerError, err.Error())
 	}
 
-	posts, err := postDao.FindByPostUserId(id,8)
+	posts, err := postDao.FindByPostUserId(id, 8)
 	if err != nil {
 		log.Println("error")
 		c.String(http.StatusInternalServerError, err.Error())
@@ -65,19 +66,20 @@ func (*UserService) GetWebEndpoint(c *gin.Context)  {
 	}
 
 	c.HTML(http.StatusOK, "userDetail.tmpl", gin.H{
-		"title": "Main website",
-		"user": user,
-		"friends": friends,
-		"items": items,
-		"posts": posts,
-		"postImages": postItems,
+		"title":        "Main website",
+		"user":         user,
+		"friendsCount": len(friends),
+		"friends":      friends[0:3],
+		"items":        items,
+		"posts":        posts,
+		"postImages":   postItems,
 	})
 }
 
 func (*UserService) ListEndpoint(c *gin.Context) {
 	type res struct {
-		Result string `json:"result"`
-		Data []models.User `json:"name"`
+		Result string        `json:"result"`
+		Data   []models.User `json:"name"`
 	}
 	limitParam := c.Query("limit")
 	limit := "100"
@@ -85,46 +87,46 @@ func (*UserService) ListEndpoint(c *gin.Context) {
 		limit = limitParam
 	}
 
-	userList,_ := userDao.FindByParam(c,limit)
-	c.JSON(http.StatusOK,res{"true",userList})
-//	resList := []res{}
-//
-//	userList, err := userDao.GetList()
-//	if err != nil {
-//		log.Fatal("error")
-//		c.String(http.StatusInternalServerError, err.Error())
-//	}
-//
-//	for _, user := range userList {
-//		r := res{Id:user.Id,Name:user.Name,Type:user.Type}
-//		resList = append(resList, r)
-//	}
-//
-//	c.JSON(http.StatusOK, resList)
-//}
-//
-//func (*UserService) ListWebEndpoint(c *gin.Context) {
-//	type res struct {
-//		Id int `json:"id"`
-//		Name string `json:"name"`
-//		Type int `json:"type"`
-//	}
-//
-//	resList := []res{}
-//
-//	userList, err := userDao.GetList()
-//	if err != nil {
-//		log.Fatal("error")
-//		c.String(http.StatusInternalServerError, err.Error())
-//	}
-//
-//	for _, user := range userList {
-//		r := res{Id:user.Id,Name:user.Name,Type:user.Type}
-//		resList = append(resList, r)
-//	}
-//
-//	c.HTML(http.StatusOK, "userList.tmpl", gin.H{
-//		"title": "Main website",
-//		"userList": resList,
-//	})
+	userList, _ := userDao.FindByParam(c, limit)
+	c.JSON(http.StatusOK, res{"true", userList})
+	//	resList := []res{}
+	//
+	//	userList, err := userDao.GetList()
+	//	if err != nil {
+	//		log.Fatal("error")
+	//		c.String(http.StatusInternalServerError, err.Error())
+	//	}
+	//
+	//	for _, user := range userList {
+	//		r := res{Id:user.Id,Name:user.Name,Type:user.Type}
+	//		resList = append(resList, r)
+	//	}
+	//
+	//	c.JSON(http.StatusOK, resList)
+	//}
+	//
+	//func (*UserService) ListWebEndpoint(c *gin.Context) {
+	//	type res struct {
+	//		Id int `json:"id"`
+	//		Name string `json:"name"`
+	//		Type int `json:"type"`
+	//	}
+	//
+	//	resList := []res{}
+	//
+	//	userList, err := userDao.GetList()
+	//	if err != nil {
+	//		log.Fatal("error")
+	//		c.String(http.StatusInternalServerError, err.Error())
+	//	}
+	//
+	//	for _, user := range userList {
+	//		r := res{Id:user.Id,Name:user.Name,Type:user.Type}
+	//		resList = append(resList, r)
+	//	}
+	//
+	//	c.HTML(http.StatusOK, "userList.tmpl", gin.H{
+	//		"title": "Main website",
+	//		"userList": resList,
+	//	})
 }
